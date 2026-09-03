b0bcf303ccc1a981391dc3664d7fb49a194b8ab0
lyncon-andrade <lyncon.andrade@aluno.cps.sp.gov.br>
2026-09-02 21:58:45 -0300

    att2

---
+++ ex5/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String[] produtos = {"Arroz", "Feijao", "Batata", "Carne", "Frango"};
+
+        for (String produto : produtos) {
+            System.out.println(produto);
+          }
+    }  
+}
